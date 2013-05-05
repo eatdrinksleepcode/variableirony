1bed9979beedca06e4b2407cc8422ffce58b5faf
David Nelson <eatdrinksleepcode@gmail.com>
2013-05-05 18:34:29 -0500

    Nuget packages

--- Terminal/Properties/AssemblyInfo.cs
+++ Terminal/Properties/AssemblyInfo.cs
@@ -1,36 +1,14 @@
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Core.Terminal")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Core.Terminal")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyTitle("VariableIrony.Terminal")]
+[assembly: AssemblyDescription("Reusable classes enabling more rapid development of console applications")]
+[assembly: AssemblyCompany("VariableIrony")]
+[assembly: AssemblyProduct("VariableIrony.Core")]
+[assembly: AssemblyCopyright("Copyright © David Nelson 2013")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("bdca218c-f791-462d-a9b5-224a7d8cbb9c")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.1")]
+[assembly: AssemblyFileVersion("0.1.1")]
+[assembly: AssemblyInformationalVersion("0.1.1-alpha")]
